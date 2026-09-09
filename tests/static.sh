dd470d2c2e4b1d10b2e4e62f00d69a5561ce3a14
Jordan Chin <jordanchin@gmail.com>
2026-09-08 19:57:18 -0500

    Launch Uvicorn with the runtime interpreter

--- tests/static.sh
+++ tests/static.sh
@@ -16,6 +16,10 @@ grep -q '^command=postgres -D /config/postgresql/data$' rootfs/etc/supervisor/su
 ! grep -q '/usr/local/bin/postgres' rootfs/etc/supervisor/supervisord.conf
 test "$(grep -c -- '--username="$POSTGRES_USER"' rootfs/usr/local/bin/start-backend)" -eq 2
 ! grep -Eq 'runuser -u postgres -- psql -|runuser -u postgres -- createdb -O' rootfs/usr/local/bin/start-backend
+grep -q '^RUN python3 -m uvicorn --version$' Dockerfile
+grep -q 'COPY --from=python-deps /install /usr/local/lib/python3.11/dist-packages' Dockerfile
+grep -q '^exec python3 -m uvicorn main:app ' rootfs/usr/local/bin/start-backend
+! grep -q 'ln -s /usr/bin/python3 /usr/local/bin/python' Dockerfile
 bash -n rootfs/usr/local/bin/aio-entrypoint rootfs/usr/local/bin/start-backend
 sh -n rootfs/usr/local/bin/aio-healthcheck
 python3 - <<'PY'
